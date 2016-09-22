fb8fdf16863eecc8bc9528a9d4281d8646e8a752
haukehein <BitCrusher@gmx-topmail.de>
2016-09-22 16:11:36 +0200

    Add files via upload

--- build.sh
+++ build.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
+
+[ -z "$( which git        2> /dev/null )" ]  &&  echo "ERROR: 'git' is not installed!"                   &&  exit 1
+[ -z "$( which docker     2> /dev/null )" ]  &&  echo "ERROR: 'docker' is not installed!"                &&  exit 1
+[ -z "$( docker --version 2> /dev/null )" ]  &&  echo "ERROR: You are not allowed to execute 'docker'!"  &&  exit 1
+
 docker build $1 -t haukehein/phusion_passenger-customizable:0.9.19_update .

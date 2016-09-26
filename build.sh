c2fd149685c2fe43013cc0a608521d2bae12d2d8
haukehein <BitCrusher@gmx-topmail.de>
2016-09-26 09:22:53 +0200

    Add files via upload

--- build.sh
+++ build.sh
@@ -3,4 +3,18 @@
 [ -z "$( which docker     2> /dev/null )" ]  &&  echo "ERROR: 'docker' is not installed!"                &&  exit 1
 [ -z "$( docker --version 2> /dev/null )" ]  &&  echo "ERROR: You are not allowed to execute 'docker'!"  &&  exit 1
 
-docker build $1 -t haukehein/phusion_passenger-customizable:0.9.19_update .
+
+  BUILD=haukehein/phusion_passenger-customizable:0.9.19_update
+#  FROM=<maintainer>/<image>:<version>
+#
+#if [ -z "$( docker images $(echo -E "$FROM" | cut -d: -f1)  2> /dev/null | grep $(echo -E "$FROM" | cut -d: -f2) )" ]
+#then
+#    TMP=`mktemp -d`
+#	git clone https://github.com/$(echo -E "$FROM" | cut -d: -f1).git "$TMP" --recursive  && \
+#	cd "$TMP"  && \
+#	bash ./build.sh
+#    cd -  
+#    rm -rf "$TMP"
+#fi
+
+docker build $1 -t $BUILD .
